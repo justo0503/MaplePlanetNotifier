7f03b0536349b73ca63accf37157fa9ba64b3a03
justo0503 <ncks2119@gmail.com>
2026-06-27 02:17:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
+import time
+
 from parser import get_latest_patch
 from notifier import send_discord
+from storage import get_last_post, save_last_post
+from config import WEBHOOK_URL, CHECK_INTERVAL
 
-WEBHOOK_URL = https://discord.com/api/webhooks/1520105869948227585/yVlojPgb0A2YpQAcxEfkGIBJrZEZZuoSPRSBpR3-UNPLX1rQl-iOaNemTGxXbrdq9h_w
 
-latest = get_latest_patch()
+def check():
 
-if latest:
+    latest = get_latest_patch()
+
+    if latest is None:
+        print("패치노트를 읽지 못했습니다.")
+        return
+
+    last = get_last_post()
+
+    if latest["id"] == last:
+        print("새 패치노트 없음")
+        return
+
+    print("새 패치노트 발견!")
+
     send_discord(WEBHOOK_URL, latest)
+
+    save_last_post(latest["id"])
+
+
+if __name__ == "__main__":
+
+    print("MaplePlanetNotifier 시작")
+
+    while True:
+
+        try:
+            check()
+
+        except Exception as e:
+            print(e)
+
+        time.sleep(CHECK_INTERVAL)
